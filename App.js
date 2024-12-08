99ec0bc4562842a2b4f0c4c6b875e0be6347a907
NIKHIL NIKAM <nikhilnnikam1997@gmail.com>
2024-12-08 12:13:22 +0530

    episode 02

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-// const hearder = React.createElement(
-//   "h1", //html "h1" tag
-//   { id: "heading", xyz: "abc" }, //attributes
-//   "Hello World! from react" // childern pass it to "h1" tag
-// );
-// console.log(hearder); // simple javascript object
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(hearder); //rendor method job is to put object with covertable into heading tag
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 🚀
+
+# Parcel
+-dev build
+-local server
+-HMR = Hot Module Replacement (automatically refreshing page)
+-file watching algorithm - written in c++
+-Caching - faster builds
+-Image optimization
+-Minifications of files
+-Bunlding of files and code
+-compressing of files and code
+-Consistent Hashing
+-code spliting
+-Differential bundling - to support older browsers and new or any browsers (nodemodules fallback)
+-Diagnostics
+-Good error shows
+-HTTPS support
+-Tree shaking - remove unused code from files
+-Different dev and Production builds

6d8deff19d6beb7fa9ac99c23daf6104c5dad025
NIKHIL NIKAM <nikhilnnikam1997@gmail.com>
2024-12-15 14:04:41 +0530

    added scripts and understands about jsx

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm an h1 tag from child 1"),
-    React.createElement("h2", {}, "I'm an h2 tag from child 1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag from child 2"),
-    React.createElement("h2", {}, "I'm an h2 tag from child 2"),
-  ]),
-]);
+const Title = () => {
+  return (
+    <div className="head" tabIndex={5}>
+      Namaste React using jsx 🚀
+    </div>
+  );
+};
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1 className="heading">Namaste React Functional Compoenent</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
